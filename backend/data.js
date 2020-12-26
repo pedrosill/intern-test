558b426dd9244e56da7fdb8e26db1a8a4fd9be05
intern222 <internpda@gmail.com>
2020-12-26 17:04:52 +0000

    Diogo user

--- backend/data.js
+++ backend/data.js
@@ -3,8 +3,8 @@ import bcrypt from 'bcryptjs';
 const data = {
     users: [
         {
-          name: 'Basir',
-          email: 'admin@example.com',
+          name: 'Diogo',
+          email: 'diogomm26@gmail.com',
           password: bcrypt.hashSync('1234', 8),
           isAdmin: true,
         },
